982d3d0ab4205647f926a622b3f438d9af39c94a
rithvikkrishn <66107089+rithvikkrishn@users.noreply.github.com>
2020-06-09 12:11:39 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,8 @@
+test_list1 = [1, 3, 4, 6, 8] 
+test_list2 = [4, 5, 6, 2, 10] 
+print ("Original list 1 : " + str(test_list1)) 
+print ("Original list 2 : " + str(test_list2))  
+list = [] 
+for i in range(0, len(test_list1)): 
+    list.append(test_list1[i] + test_list2[i]) 
+print ("the list is : " + str(list)) 
