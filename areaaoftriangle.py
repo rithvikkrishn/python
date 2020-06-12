391e2cc1c6f6e3a2cc718d71796643c62641936e
rithvikkrishn <66107089+rithvikkrishn@users.noreply.github.com>
2020-06-12 12:12:29 +0530

    Add files via upload

---
+++ areaaoftriangle.py
@@ -0,0 +1,4 @@
+PI = float(input('Enter the radius of the circle :'))
+r = float(input('Enter the radius of the circle :'))
+area = PI * r * r
+print("Area of the circle is : %.2f" %area)
---
+++ reverse.py
@@ -0,0 +1,4 @@
+a = ['Windows', 'macOS', 'Linux']
+print('Original List:', a)
+a.reverse()
+print('Updated List:', a)
