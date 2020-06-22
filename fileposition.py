56dd1cf7c997816642c336e0687e0fbfa7b4c170
rithvikkrishn <66107089+rithvikkrishn@users.noreply.github.com>
2020-06-22 11:03:01 +0530

    Add files via upload

---
+++ fileposition.py
@@ -0,0 +1,9 @@
+fo = open("hello.txt", "r+")
+str = fo.read(10)
+print("Read String is : ", str)
+position = fo.tell()
+print("Current file position : ", position)
+position = fo.seek(0, 0);
+str = fo.read(10)
+print("Again read String is : ", str)
+fo.close()
---
+++ filerename.py
@@ -0,0 +1,2 @@
+import os
+os.rename( "ex1.txt","hello.txt")
